366cfa6b18a009b4b7011142eacfe0d76ad7bd37
Dan <scandox@gmail.com>
2015-06-08 01:15:00 +0100

    Changing response function to implement all the prerequisite actions prior to sending of actual response

--- apps/gateway_vpn_service/lib/server/response.ex
+++ apps/gateway_vpn_service/lib/server/response.ex
@@ -1,18 +1,26 @@
 defmodule GatewayVPNService.Server.Response do
   import Application
   alias GatewayVPNService.Crypto
+  alias GatewayVPNService.VPN.Server, as: VPNServer
 
   @doc "Takes in an authenticated request and generates required response"
   def create(request) when is_map(request) do
     gateway_id = request["gateway_id"]
+    vpn_username =  "gateway#{gateway_id}" 
+    vpn_local_hostname = "gateway#{gateway_id}.#{get_env(:gateway_vpn_service, :vpn_hostname)}"
     {:ok, key, certificate} = Crypto.generate_key_pair(gateway_id)
+    {:ok, mac_address} = Crypto.generate_mac_address
+    {:ok, certificate_path} = Crypto.store_certificate(certificate, gateway_id)
+    {:ok, 0} = VPNServer.user_create(vpn_username)
+    {:ok, 0} = VPNServer.user_cert_set(certificate_path)
+    {:ok, 0} = DHCP.add_static_lease(DHCP.get_free_ip_address(vpn_local_hostname), mac_address, vpn_local_hostname)
 
-    %{:vpn_username => "gateway#{gateway_id}",
+    %{:vpn_username => vpn_username,
       :vpn_hub => get_env(:gateway_vpn_service, :vpn_hub), 
       :vpn_port => get_env(:gateway_vpn_service, :vpn_port), 
       :vpn_hostname => get_env(:gateway_vpn_service, :vpn_hostname),
-      :vpn_local_hostname => "gateway#{gateway_id}.#{get_env(:gateway_vpn_service, :vpn_hostname)}", 
-      :vpn_mac_address => Crypto.generate_mac_address,
+      :vpn_local_hostname =>  vpn_local_hostname,
+      :vpn_mac_address => mac_address,
       :vpn_private_key => key,
      :vpn_certificate => certificate
     }
