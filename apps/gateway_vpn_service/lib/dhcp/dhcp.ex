bbc2e1f0713a88f7db15a288334c3d470c54beb4
Daniel Caffrey <scandox@gmail.com>
2015-06-10 22:45:06 +0100

    Changed IP range constants to be integers from the get-go. No point in having to convert them and also it means I get to do this: added guard clause to get_free_ip_address to ensure that once upper limit is reached the process will fail. In other words if we run out of IP addresses we don't give any out anymore. Also changed reserve_ip_address to deal with the fact that Redis return values are strings not ints. Changed get_all_gateway_ips to handle an empty set (i.e. initial state) and converted each one in results to a tuple style IP address. Added notes to omapi? function as the return values are kind of counter-intuitive.

--- apps/gateway_vpn_service/lib/dhcp/dhcp.ex
+++ apps/gateway_vpn_service/lib/dhcp/dhcp.ex
@@ -2,12 +2,12 @@ defmodule GatewayVPNService.DHCP do
   import Gateway.Utilities.External
   import Application
   alias Gateway.Utilities.Network, as: NetUtils
-  alias Gateway.VPNService.Redis 
+  alias GatewayVPNService.Redis 
   require Logger
 
   # This is the range of allowed IP Addresses for Gateways on the VPN
-  @ip_range_start {10,44,208,47}
-  @ip_range_end {10,44,223,207}
+  @ip_range_start 170709039 #{10,44,208,47}
+  @ip_range_end 170713039 #{10,44,223,207}
 
   @doc "Calls a python script that can a static lease host entry in the
   DHCPD configuration of the server"
@@ -30,7 +30,7 @@ defmodule GatewayVPNService.DHCP do
       |> get_free_ip_address
   end
 
-  def get_free_ip_address(current_ip) when is_integer(current_ip) do
+  def get_free_ip_address(current_ip) when is_integer(current_ip) and current_ip < @ip_range_end do
     if omapi?(current_ip) do
       get_free_ip_address(current_ip+1)
     else
@@ -41,7 +41,7 @@ defmodule GatewayVPNService.DHCP do
   # Tries to reserve the ip address in question. If it succeeds then it 
   # returns it, if not it starts incrementing again 
   defp reserve_ip_address(ip) when is_integer(ip) do
-    ip_address = NetUtils.to_ipstring(ip)
+    ip_address = ip |> NetUtils.to_ipaddress |> NetUtils.to_ipstring
     if !reserve_ip_address(ip_address) do
       get_free_ip_address(ip+1)
     else
@@ -52,7 +52,7 @@ defmodule GatewayVPNService.DHCP do
   # Tries to add as key to REDIS set
   defp reserve_ip_address(ip) when is_binary(ip) do
     {:ok, result} = Redis.query ["SADD",vpn_server_redis_key, ip]
-    if result == 1 do
+    if result == "1" do
       true
     else
       false
@@ -62,20 +62,24 @@ defmodule GatewayVPNService.DHCP do
   # Retrieves list of all gateway ips from REDIS K-V store 
   defp get_all_gateway_ips do
     {:ok, ips} = Redis.query ["SMEMBERS",vpn_server_redis_key]
-    ips
+    if Enum.empty?(ips), do: ips = [@ip_range_start |> NetUtils.to_ipaddress |> NetUtils.to_ipstring]
+    ips |> Enum.map(fn(x) -> NetUtils.to_ipaddress(x) end)
   end
 
+  # Runs an OMAPI script which queries DHCP server to see if an IP has been assigned
+  # This is to ensure we don't collide with properly dynamic leases issued to VPN
+  # clients
   defp omapi?(ip_address) do
     command = shell("#{omapi_script} lookup #{ip_address}")
     Logger.info(command.out)
     case command.status do
       0 ->
-        true
+        false # Found no entry
       1 ->
-        false
+        true # Found an existing entry
       _ ->
         Logger.error("Error looking up OMAPI: #{command.out} #{command.status}")
-        false
+        true # Let's assume there is an entry rather than risk collision
     end
  end
 
